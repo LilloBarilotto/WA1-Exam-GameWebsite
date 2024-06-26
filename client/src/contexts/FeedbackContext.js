8fe4b717bef629282e69d5b6f111e4566ec3a9e6
SEBASTIANO GUZZONE s322848 <s322848@studenti.polito.it>
2024-06-26 15:50:09 +0200

    Removed Context

--- client/src/contexts/FeedbackContext.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const FeedbackContext = React.createContext({
-    setFeedback: (message) => {},
-    setFeedbackFromError: (error) => {},
-    setShouldRefresh: (value) => {}
-});
-
-export default FeedbackContext;
